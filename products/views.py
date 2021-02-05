7f7b7a78c5422b944fdc17a4030d98dcdefe3693
Miriam Gurrieri <mion93@hotmail.it>
2021-02-05 15:05:55 +0000

    added product-detail template

--- products/views.py
+++ products/views.py
@@ -13,3 +13,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
