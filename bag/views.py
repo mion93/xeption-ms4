3a504fa7c75b4f6c2032996f4ed653fcdf8b8864
Miriam Gurrieri <mion93@hotmail.it>
2021-02-11 13:38:59 +0000

    added adjust bag and remove bag from views and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 # Create your views here.
 
 def view_bag(request):
@@ -21,4 +22,37 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-    
+
+
+def adjust_bag(request, item_id):
+    """Update the quantity in the bag"""
+
+    quantity = int(request.POST.get('quantity'))
+    product = Product.objects.get(pk=item_id)
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request, f'Updated quantity')
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag!')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}.')
+        return HttpResponse(status=500)
