4bc574b4022931538bfb9fcd648a153ef2355442
Miriam Gurrieri <mion93@hotmail.it>
2021-02-15 21:19:21 +0000

    added stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,27 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#bc7b6a',
+        iconColor: '#bc7b6a'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ILDTvF3dQIR3GNaFamVxCpaqSWMkZZ4xzwLB0PqdTrCWgJEsmeh7zclaMZDrLGsPUyeGa4rsY60VVo37ME0Jm1R00LTLzNX9V',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
