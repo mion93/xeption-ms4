bf7d18b83164a12d28056746944af1d8a0ef5e7a
Miriam Gurrieri <mion93@hotmail.it>
2021-02-16 19:28:48 +0000

    completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
