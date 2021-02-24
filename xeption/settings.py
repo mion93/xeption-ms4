cb3cbc5a9d0d881076651fd860609a7dacae28fc
Miriam Gurrieri <mion93@hotmail.it>
2021-02-24 11:30:14 +0000

    deployment part 1

--- xeption/settings.py
+++ xeption/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,6 +126,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
