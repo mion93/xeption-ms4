4a3c2101c323a04eeb6685a858b0be4bfe4bf569
Miriam Gurrieri <mion93@hotmail.it>
2021-02-24 18:48:24 +0000

    added cache control

--- xeption/settings.py
+++ xeption/settings.py
@@ -176,6 +176,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'xeption-ms4'
     AWS_S3_REGION_NAME = 'eu-west-2'
